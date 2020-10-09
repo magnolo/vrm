4d4c0740d557221f49e97cc23eee53783dfde8be
Magnolo <walder.manfred@gmail.com>
2020-10-09 12:25:29 +0200

    pubm ver

---
+++ dist/vfa-slider/README.md
@@ -0,0 +1,26 @@
+# Vfa Slider
+
+## Install
+
+Run `yarn add vfa-slider @pqina/flip` 
+
+## Use
+
+Import `import { VfaSliderModule } from 'vfa-slider'` to your `app.modules.ts`
+
+
+```
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    VfaSliderModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+```
+
+`<vfa-slider></vfa-slider>`
---
+++ dist/vfa-slider/esm2015/lib/vfa-slider.service.js
@@ -0,0 +1,14 @@
+import { __decorate } from "tslib";
+import { Injectable } from '@angular/core';
+import * as i0 from "@angular/core";
+let ComponentsService = class ComponentsService {
+    constructor() { }
+};
+ComponentsService.ɵprov = i0.ɵɵdefineInjectable({ factory: function ComponentsService_Factory() { return new ComponentsService(); }, token: ComponentsService, providedIn: "root" });
+ComponentsService = __decorate([
+    Injectable({
+        providedIn: 'root'
+    })
+], ComponentsService);
+export { ComponentsService };
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidmZhLXNsaWRlci5zZXJ2aWNlLmpzIiwic291cmNlUm9vdCI6Im5nOi8vdmZhLXNsaWRlci8iLCJzb3VyY2VzIjpbImxpYi92ZmEtc2xpZGVyLnNlcnZpY2UudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLE9BQU8sRUFBRSxVQUFVLEVBQUUsTUFBTSxlQUFlLENBQUM7O0FBSzNDLElBQWEsaUJBQWlCLEdBQTlCLE1BQWEsaUJBQWlCO0lBRTVCLGdCQUFnQixDQUFDO0NBQ2xCLENBQUE7O0FBSFksaUJBQWlCO0lBSDdCLFVBQVUsQ0FBQztRQUNWLFVBQVUsRUFBRSxNQUFNO0tBQ25CLENBQUM7R0FDVyxpQkFBaUIsQ0FHN0I7U0FIWSxpQkFBaUIiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBJbmplY3RhYmxlIH0gZnJvbSAnQGFuZ3VsYXIvY29yZSc7XG5cbkBJbmplY3RhYmxlKHtcbiAgcHJvdmlkZWRJbjogJ3Jvb3QnXG59KVxuZXhwb3J0IGNsYXNzIENvbXBvbmVudHNTZXJ2aWNlIHtcblxuICBjb25zdHJ1Y3RvcigpIHsgfVxufVxuIl19
