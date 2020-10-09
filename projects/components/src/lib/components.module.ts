e11c51d970b5e237f2255c8f13c305a1825e8bc4
Magnolo <walder.manfred@gmail.com>
2020-10-09 11:26:44 +0200

    build update

--- projects/components/src/lib/components.module.ts
+++ projects/components/src/lib/components.module.ts
@@ -2,8 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ComponentsComponent } from './components.component';
 
-
-
 @NgModule({
   declarations: [ComponentsComponent],
   imports: [
